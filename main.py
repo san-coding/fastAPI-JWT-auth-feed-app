86630e63f139790ba2ebf618d132a1814704662f
Sandeep Rajakrishnan <sandur43@gmail.com>
2022-02-03 00:57:49 +0530

    setup fastAPI endpoints to create and get posts

---
+++ main.py
@@ -0,0 +1,37 @@
+import uvicorn
+from fastapi import FastAPI
+from app.model import PostSchema
+
+posts = [{"id": 1, "title": "Post Demo", "content": "This is a demo post"}]
+
+users = []
+
+app = FastAPI()
+
+# get - for testing
+@app.get("/", tags=["test"])
+def greet():
+    return {"message": "Hello World"}
+
+
+# get posts
+@app.get("/posts", tags=["posts"])
+def get_all_posts():
+    return {"All posts": posts}
+
+
+# get posts by id
+@app.get("/posts/{id}", tags=["posts"])
+def get_post_by_id(id: int):
+    if id > len(posts):
+        return {"error": "Post not found"}
+    else:
+        return {"Post": posts[id - 1]}
+
+
+# create a post using websokects
+@app.post("/posts", tags=["posts"])
+def create_post(post: PostSchema):
+    post.id = len(posts) + 1
+    posts.append(post.dict())
+    return {"info": "Post created successfully with id: " + str(post.id)}
