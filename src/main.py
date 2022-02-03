d2163e74f91029a11aef7eea2c2a6262f9103d70
Sandeep Rajakrishnan <sandur43@gmail.com>
2022-02-03 20:27:01 +0530

    storing and accessing data from mongoDB

--- src/main.py
+++ src/main.py
@@ -1,31 +1,88 @@
 from fastapi import FastAPI, Depends, HTTPException
 from .auth import AuthHandler
 from .schemas import AuthDetails, PostSchema
-
+from pymongo import MongoClient
+from decouple import config
 
 app = FastAPI()
 
 
 auth_handler = AuthHandler()
 users = []
+posts = []
+print("Connecting to mongo db")
+URI = config("MONGODB_URI")
+client = MongoClient(URI)
 
-posts = [
-    {
-        "id": 1,
-        "title": "First Post",
-        "content": "Edvora is a cool place to work",
-        "author": "Sandeep Rajakrishnan",
-    }
-]
+# send posts to mongo db cluster
+def send_posts():
+
+    db = client.fastapiJWT
+    collection = db.posts
+    # only insert if post id not in db
+    for post in posts:
+        if collection.find_one({"id": post["id"]}) is None:
+            collection.insert_one(post)
 
 
+# send users to mongo db cluster
+def send_users():
+    db = client.fastapiJWT
+    collection = db.users
+    # only insert if user id not in db
+    for user in users:
+        if collection.find_one({"username": user["username"]}) is None:
+            collection.insert_one(user)
+
+
+def get_posts():
+    db = client.fastapiJWT
+    collection = db.posts
+    return collection.find()
+
+
+def get_users():
+    db = client.fastapiJWT
+    collection = db.users
+    return collection.find()
+
+
+# storing posts in memory from mongo db
+post_files = get_posts()
+for file in post_files:
+    # add only id, title, content, author to posts
+    posts.append(
+        {
+            "id": file["id"],
+            "title": file["title"],
+            "content": file["content"],
+            "author": file["author"],
+        }
+    )
+
+# storing users in memory from  mongo db
+user_files = get_users()
+for file in user_files:
+    # add only name, username, password to users
+    users.append(
+        {
+            "name": file["name"],
+            "username": file["username"],
+            "password": file["password"],
+        }
+    )
+
+# homepage
 @app.get("/", tags=["Home page"])
 def home():
+    # return html and json as response
+
     return {"hello": "Welcome to feed app"}
 
 
+# user registration
 @app.post("/register", status_code=201, tags=["User Authentication"])
-def register_user(auth_details: AuthDetails):
+async def register_user(auth_details: AuthDetails):
     if any(x["username"] == auth_details.username for x in users):
         raise HTTPException(
             status_code=400,
@@ -39,9 +96,11 @@ def register_user(auth_details: AuthDetails):
             "password": hashed_password,
         }
     )
+    send_users()
     return {"info": "User registered successfully"}
 
 
+# user login
 @app.post("/login", tags=["User Authentication"])
 def login_user(auth_details: AuthDetails):
     user = None
@@ -58,13 +117,23 @@ def login_user(auth_details: AuthDetails):
     return {"token": token}
 
 
+# current logged in user details
 @app.get("/logged_in_user", tags=["User Authentication"])
 def logged_in_user(username=Depends(auth_handler.auth_wrapper)):
-    # extract name from username
     for user in users:
         if user["username"] == username:
-            return {"info": "Logged in as " + user["name"]}
+            return {"info": "Logged in as " + user["name"] + ". Email: " + username}
     raise HTTPException(status_code=404, detail="User not found")
+
+
+# create posts by authenticated users
+@app.post("/create_post", tags=["Posts"])
+async def create_post(post: PostSchema, username=Depends(auth_handler.auth_wrapper)):
+    post.id = len(posts) + 1
+    post.author = username
+    posts.append(post.dict())
+    send_posts()
+    return {"info": "Post successfully created by " + username}
 
 
 # get all the posts
@@ -77,17 +146,23 @@ def get_all_posts():
 
 # get posts by id
 @app.get("/posts/{id}", tags=["Posts"])
-def get_post_by_id(id: int):
+def get_post_by_id(id: int, username=Depends(auth_handler.auth_wrapper)):
     if id > len(posts):
         return {"error": "Post not found"}
     else:
         return {"Post": posts[id - 1]}
 
 
-# create posts by authenticated users
-@app.post("/create_post", tags=["Posts"])
-def create_post(post: PostSchema, username=Depends(auth_handler.auth_wrapper)):
-    post.id = len(posts) + 1
-    post.author = username
-    posts.append(post.dict())
-    return {"info": "Post successfully created by " + username}
+# get posts by username
+@app.get(
+    "/posts/user/{username}",
+    tags=["Posts"],
+)
+def get_post_by_username(user: str, username=Depends(auth_handler.auth_wrapper)):
+    if len(posts) == 0:
+        return {"info": "No posts found"}
+    user_posts = []
+    for post in posts:
+        if post["author"] == username:
+            user_posts.append(post)
+    return {" posts": user_posts}
